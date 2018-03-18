f0babd7389914d981cc3d915cd4ba689159b47d8
이수한 <soohan@isuhan-ui-macbook-pro.local>
2018-03-18 16:22:49 +0900

    Initial Commit

---
+++ FlagQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlagQuiz
+//
+//  Created by 이수한 on 2018. 3. 18..
+//  Copyright © 2018년 이수한. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
