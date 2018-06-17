dfdb301e4bc5cb87b62554dc7d36610f9e80de6c
이수한 <soohan@isuhan-ui-macbook-pro.local>
2018-06-18 08:47:24 +0900

    add detail country information view

---
+++ FlagQuiz/DetailCountryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailCountryViewController.swift
+//  FlagQuiz
+//
+//  Created by 이수한 on 2018. 6. 18..
+//  Copyright © 2018년 이수한. All rights reserved.
+//
+
+import UIKit
+
+class DetailCountryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
