be5fdcfdadcec4d6cc260bddf90ed4bce7a25f49
Soo-Han Lee <martino_lee@naver.com>
2018-08-22 22:07:44 +0900

    Create animation class

---
+++ FlagQuiz/AnimUtils.swift
@@ -0,0 +1,13 @@
+//
+//  AnimUtils.swift
+//  FlagQuiz
+//
+//  Created by 이수한 on 2018. 8. 22..
+//  Copyright © 2018년 이수한. All rights reserved.
+//
+
+import UIKit
+
+class AnimUtils: NSObject {
+    
+}
