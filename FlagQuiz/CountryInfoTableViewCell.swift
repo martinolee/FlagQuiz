01ecf4ec5d63cd68335eba343394c6ea11ae38e4
Soo-Han Lee <martino_lee@naver.com>
2019-06-11 21:51:15 +0800

    Optimized CountryInfoTableViewCell UI for iPad and iPhone

--- FlagQuiz/CountryInfoTableViewCell.swift
+++ FlagQuiz/CountryInfoTableViewCell.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class CountryInfoTableViewCell: UITableViewCell {
     @IBOutlet weak var flagImageView: UIImageView!
-    @IBOutlet weak var countryNameLabel: UILabel!
     
     @IBOutlet var flagImageViewHeight: NSLayoutConstraint!
     @IBOutlet var flagImageViewWidth: NSLayoutConstraint!
+    
+    @IBOutlet var flagImageViewLeftConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var countryNameLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,9 +24,15 @@ class CountryInfoTableViewCell: UITableViewCell {
         if ( UIDevice.current.userInterfaceIdiom == .phone ) {
             flagImageViewHeight.constant = 100
             flagImageViewWidth.constant = 150
+            
+            countryNameLabel.font = countryNameLabel.font.withSize(17)
         } else if ( UIDevice.current.userInterfaceIdiom == .pad ) {
             flagImageViewHeight.constant = 200
             flagImageViewWidth.constant = 300
+            
+            flagImageViewLeftConstraint.constant = 50
+            
+            countryNameLabel.font = countryNameLabel.font.withSize(34)
         }
     }
 
