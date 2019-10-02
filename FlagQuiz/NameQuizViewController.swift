e450300cc5bd72365b65276a33a8e2d36d45b53e
Soo-Han Lee <martino_lee@naver.com>
2019-10-02 21:05:19 +0900

    Make connections between UI and codes

--- FlagQuiz/NameQuizViewController.swift
+++ FlagQuiz/NameQuizViewController.swift
@@ -7,9 +7,47 @@
 //
 
 import Foundation
+import UIKit
+import GoogleMobileAds
 
 class NameQuizViewController: QuizViewController {
+    
+    @IBOutlet weak var countryNameLabel: UILabel!
+    
+    @IBOutlet weak var leftTopButton: UIButton!
+    @IBOutlet weak var rightTopButton: UIButton!
+    @IBOutlet weak var leftBottomButton: UIButton!
+    @IBOutlet weak var rightBottomButton: UIButton!
+    
+    @IBOutlet weak var firstLife: UIImageView!
+    @IBOutlet weak var secondLife: UIImageView!
+    @IBOutlet weak var thirdLife: UIImageView!
+    @IBOutlet weak var fourthLife: UIImageView!
+    @IBOutlet weak var fifthLife: UIImageView!
+    
+    var buttonArray: Array<UIButton> = []
+    var lifeArray: Array<UIImageView> = []
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var correctOrIncorrectView: UIView!
+    @IBOutlet weak var correctOrIncorrectLabel: UILabel!
+    
+    @IBOutlet var difficultyNotificationView: UIView!
+    @IBOutlet var difficultyLabel: UILabel!
+    
+    @IBOutlet var bannerView: GADBannerView!
+    
     override func displayQuestion() {
         print("NameQuizViewController.displayQuestion()")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bannerView.adUnitID = bannerAdUnitID
+        bannerView.rootViewController = self
+        
+        bannerView.load(GADRequest())
     }
 }
