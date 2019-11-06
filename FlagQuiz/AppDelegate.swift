f07a04cad496971b74d6cd82f64ab845c8a33103
Soo-Han Lee <martino_lee@naver.com>
2019-11-06 11:17:29 +0900

    Update Google Mobile Ads library to 7.51.0

--- FlagQuiz/AppDelegate.swift
+++ FlagQuiz/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GADMobileAds.configure(withApplicationID: appId)
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
         
         return true
     }
